0c6a735081235f8fe7b62b1f6216394900956937
Joseph Toups <josephtoups95@gmail.com>
2021-05-04 23:27:09 -0700

    adding seeds

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+Insert into department (dept_name) values ('Finance'), ('Engineering'), ('Legal');
+Insert	into	company_role (title, salary, department_id) values
+('Analyst', 60000.00, 1),
+('Associate Analyst', 70000.00, 1),
+('Senior Analyst', 80000.00, 1),
+('Engineer', 70000.00, 2),
+('Associate Engineer', 80000.00, 2),
+('Senior Engineer', 90000.00, 2),
+('Junior Attorney', 70000.00, 3),
+('Associate Attorney', 80000.00, 3),
+('Senior Associate', 90000.00, 3);
