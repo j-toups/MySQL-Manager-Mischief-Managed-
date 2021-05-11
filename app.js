ed75aadbda9d2be553fa992bcf313d7718f2052d
Joseph Toups <josephtoups95@gmail.com>
2021-05-11 16:55:33 -0700

    adding gitignore and fixing schema

--- app.js
+++ app.js
@@ -4,46 +4,45 @@ require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: '3006',
+    port: '3306',
     user: 'root',
     password: 'password',
     database: 'company_db'
 });
 
-connection.connect(function(err) {
+connection.connect((err) => {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    exports.start();
-  });
-  
-  exports.start = () => {
-      inq.prompt([
-          {
-              type: "list",
-              message: "What would you like to do?",
-              name: "choice",
-              choices: [
-                  "View All Employees",
-                  "Add Employee",
-                  "Update Employee Role",
-                  "EXIT"                
-              ]
-          }
-      ])
-      .then(function(answer) {
-        if(answer.choice === "View All Employees") {
-          view.viewAllEmployees();
+    console.log('connected');
+    start();
+});
+
+exports.start = () => {
+    inq.prompt([
+        {
+            type: 'list"',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'EXIT'                
+            ]
         }
-        else if(answer.choice === "Add Employee") {
-          add.addEmployee();
+    ])
+    .then(function(answer) {
+        if(answer.choice === 'View All Employees') {
+            view.viewAllEmployees();
+        }
+        else if(answer.choice === 'Add Employee') {
+            add.addEmployee();
         }      
-        else if(answer.choice === "Update Employee Role") {
-          update.updateRole();
+        else if(answer.choice === 'Update Employee Role') {
+            update.updateRole();
         }
-        else if(answer.choice === "EXIT") {
-          connection.end();
-          return
+        else if(answer.choice === 'EXIT') {
+            connection.end();
+            return
         }
-      });
-      
-  };
+    });
+};
--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,14 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-	department_id int not null auto_increment primary key,
-    department_name varchar(30)
+	department_id int not null auto_increment,
+    department_name varchar(30),
+    primary key (department_id)
     );
 
 CREATE TABLE company_role (
-	role_id int not null auto_increment primary key,
+	role_id int not null auto_increment,
+    primary key (role_id),
     title varchar(30) not null, 
     salary decimal not null, 
     department_id int,
@@ -17,10 +19,12 @@ CREATE TABLE company_role (
 	);
 
 CREATE TABLE employees (
-	employee_id int not null auto_increment primary key,
+    employee_id int not null auto_increment,
+    primary key(employee_id),
     first_name varchar(30) not null, 
     last_name varchar(30) not null, 
     employee_role_id int not null, 
+    manager_id int, 
     foreign key (employee_role_id) references company_role(role_id),
     foreign key (manager_id) references employees(employee_id)
 );
